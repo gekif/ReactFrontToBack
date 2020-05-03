ead2b1f8f257080901294bb8e7025ddd2bba2c33
Gekif <dzulfikar.maulana@gmail.com>
2020-05-03 16:39:53 +0700

    4. Events, Passing Props, React Router & More: 8. Repos & RepoItem Component & Data

--- github-finder/src/components/users/User.js
+++ github-finder/src/components/users/User.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -7,13 +8,16 @@ import { Link } from 'react-router-dom';
 class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired
+        repos: PropTypes.array.isRequired,
+        getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
     };
 
 
@@ -34,7 +38,7 @@ class User extends Component {
             hireable
         } = this.props.user;
 
-        const { loading } = this.props;
+        const { loading, repos } = this.props;
 
         if (loading) return <Spinner />;
 
@@ -108,8 +112,10 @@ class User extends Component {
                         Public Gists: { public_gists }
                     </div>
                 </div>
+
+                <Repos repos={ repos } />
             </Fragment>
-        )
+        );
     }
 }
 
