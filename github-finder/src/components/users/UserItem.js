ab786762e48e2eb551b619efcd8e9634218a3e84
Gekif <dzulfikar.maulana@gmail.com>
2020-05-03 15:50:08 +0700

    4. Events, Passing Props, React Router & More: 6. Single User Component & Data

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+
 
 const UserItem = ({ user: { login, avatar_url, html_url }}) => {
     return (
@@ -14,17 +16,19 @@ const UserItem = ({ user: { login, avatar_url, html_url }}) => {
             <h3>{ login }</h3>
 
             <div>
-                <a href={ html_url }
+                <Link to={`/user/${login}`}
                    className="btn btn-dark btn-sm my-1">
                     More
-                </a>
+                </Link>
             </div>
         </div>
     );
 };
 
+
 UserItem.propTypes = {
     user: PropTypes.object.isRequired
 };
+
 
 export default UserItem;
